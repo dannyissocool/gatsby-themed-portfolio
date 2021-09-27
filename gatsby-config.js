c504d55fb1f67e62400e0ab5c029def91089944b
Danny Meuse <danny.meuse@gmail.com>
2021-09-26 19:19:34 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "dan-portolio-gatsby-themed",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
